b9e7476b3e6c8e5591cabd5963ae9323526a97e8
Frank0125 <a01252974@tec.mx>
2024-05-09 11:10:30 -0700

    Corrección: Ya no salta error si ya esta el archivo encriptado

--- Encriptador/encriptador.bat
+++ Encriptador/encriptador.bat
@@ -4,4 +4,8 @@ setlocal enabledelayedexpansion
 set "folder_to_zip=%~1"
 set "zip_file_path=..\archivos_encriptados.zip"
 
+if exist "%zip_file_path%" (
+    del "%zip_file_path%"
+)
+
 powershell -Command "Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::CreateFromDirectory('%folder_to_zip%', '%zip_file_path%')"
